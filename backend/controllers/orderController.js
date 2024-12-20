74100ad693cca4081d10f5f1d2829335487552e4
veeraj2404 <virajwasnik24@gmail.com>
2024-12-20 17:27:38 +0530

    order page is ready for admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -85,7 +85,17 @@ const userOrders = async (req, res) => {
 //Update order status
 const updateStatus = async (req, res) => {
 
+    try {
 
+        const { orderId, status } = req.body;
+
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({ success: true, message: 'Status Updated' })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
